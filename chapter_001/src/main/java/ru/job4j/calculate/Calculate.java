f24bd29458a135e07fa66cb5f40e9d9d54ba6526
Evgeny Sh <xxx-zet-xxx@yandex.ru>
2018-11-06 14:11:04 +0300

    1.3. JavaDoc. Code convention[#79709]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,20 @@
 package ru.job4j.calculate;
 
+/**
+ * сlass Calculate
+ *
+ * @author Evgeny Shevchenko (xxx-zet-xxx@yandex.ru)
+ * @version 001
+ * @since 06.11.2018
+ */
 public class Calculate {
 	
-	public static void main(String[] args) {
-		
-		System.out.println("Hello world");
-		
+	/**
+	 * method main
+	 * prints "Hello world" in console.
+	 * param args - args.
+	 */
+	public static void main(String[] args) {	
+		System.out.println("Hello world");		
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Evgeny Shevchenko (xxx-zet-xxx@yandex.ru)
+* @version 001
+* @since 06.11.2018
+*/
+package ru.job4j.calculate;
