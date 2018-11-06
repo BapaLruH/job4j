ab01a9fd8edbf7381674568754dbda4bbe11ba8c
Evgeny Sh <xxx-zet-xxx@yandex.ru>
2018-11-06 18:43:43 +0300

    1.5. Checkstyle[#79713]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,27 +1,29 @@
 package ru.job4j.calculate;
 
 /**
- * сlass Calculate
+ * Сlass Calculate.
  *
  * @author Evgeny Shevchenko (xxx-zet-xxx@yandex.ru)
  * @version 001
  * @since 06.11.2018
  */
 public class Calculate {
-	/**
-	 * Method main
-	 * Prints "Hello world" in console.
-	 * @param args - args.
-	 */
-	public static void main(String[] args) {	
-		System.out.println("Hello world");		
-	}
-	/**
-	 * Method echo.
-	 * @param name Evgeny Shevchenko.
-	 * @return Echo plus your name.
-	 */
-	public String echo(String name) {
-		return "Echo, echo, echo : " + name;
-	}
+    /**
+     * Method main.
+     * Prints "Hello world" in console.
+     *
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    /**
+     * Method echo.
+     * @param name Evgeny Shevchenko.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
