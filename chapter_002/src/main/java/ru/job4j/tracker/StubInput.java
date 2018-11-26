1072dc0301fe70825d31dbd7683f0060e6e810f8
Evgeny Sh <xxx-zet-xxx@yandex.ru>
2018-11-26 14:09:32 +0300

    3. if-else-throw[#79798]

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -46,10 +46,9 @@ public class StubInput implements Input {
                 break;
             }
         }
-        if (exist) {
-            return key;
-        } else {
+        if (!exist) {
             throw new MenuOutException("Out of menu range");
         }
+        return key;
     }
 }
