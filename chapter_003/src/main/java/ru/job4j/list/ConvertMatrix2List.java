e43e78b29387c35ee41f60ca35c1f91bd54ef3a0
Evgeny Sh <xxx-zet-xxx@yandex.ru>
2018-11-29 19:45:11 +0300

    3. Конвертация двумерного массива в ArrayList[#79760]

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -0,0 +1,31 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Сlass ConvertMatrix2List.
+ *
+ * @author Evgeny Shevchenko (xxx-zet-xxx@yandex.ru)
+ * @version 001
+ * @since 29.11.2018
+ */
+public class ConvertMatrix2List {
+    /**
+     * Method toList.
+     * Converting a matrix into a list.
+     *
+     * @param array type int[][].
+     * @return list type List<Integer>.
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] row : array) {
+            for (int number : row) {
+                list.add(number);
+            }
+        }
+        return list;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ConvertMatrix2ListTest {
+
+    @Test
+    public void when2on2MatrixThenList4() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when4on5MatrixThenList20() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16},
+                {17, 18, 19, 20}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
