6bfa1b81ee6e7e43d3113a5b70debb6c41d15aaf
Evgeny Sh <xxx-zet-xxx@yandex.ru>
2018-12-20 21:27:19 +0300

    2. Не перекрывать equals hashCode[#79813]

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -1,7 +1,6 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
-import java.util.Objects;
 
 public class User {
 
---
+++ chapter_005/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertFalse;
+
+public class UserTest {
+    @Test
+    public void whenNotOverrideEqualsAndHashCode() {
+        User first = new User("Fedor", 1, new GregorianCalendar(25, Calendar.FEBRUARY, 1991));
+        User second = new User("Fedor", 1, new GregorianCalendar(25, Calendar.FEBRUARY, 1991));
+        Map<User, String> users = new HashMap<>();
+        users.put(first, "first");
+        users.put(second, "second");
+        System.out.println(users);
+        assertFalse(first.equals(second));
+    }
+}
